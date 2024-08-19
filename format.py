6ab82d7e8a455167f9d273111548d0f12c6a1391
JobearAlMahomud <jobear15-2174@diu.edu.bd>
2024-08-19 12:02:28 +0600

    added file format

---
+++ format.py
@@ -0,0 +1,8 @@
+#String Formatting
+num1 = 20
+num2 = 10
+
+username = 'Jobear'
+Roll = '12'
+print(f'My name is : {username} & my Roll is : {Roll} ')
+print(f'This is my super number : {num1 + num2}')
