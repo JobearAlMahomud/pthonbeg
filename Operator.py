3f109e9e31c71b3584f01bbafc648407831c1117
JobearAlMahomud <jobear15-2174@diu.edu.bd>
2024-08-22 10:06:46 +0600

    Arithmetig Finished

--- Operator.py
+++ Operator.py
@@ -1,3 +1,26 @@
-a = 10
-b = 20
+a = 5
+b = 2
+
+#Addition
 print(a + b)
+#Subtraction
+print(a-b )
+#Multiplication
+print(a*b)
+#Divition
+print(a/b)
+
+#Modulus
+print(a % b)
+
+#Exponetiation
+#10*10*10
+x=10
+y=3
+print( x ** y)
+
+#floor division
+#vagses kete dibe
+sum1 = 15
+sum2 = 2
+print(sum1//sum2)
