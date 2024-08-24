f07a0a7ea950966010b189286b59e45bacb7a539
JobearAlMahomud <jobear15-2174@diu.edu.bd>
2024-08-24 12:11:19 +0600

    swap

--- Operator.py
+++ Operator.py
@@ -1,3 +1,6 @@
+
+#Arithmetic Operators
+
 a = 5
 b = 2
 
@@ -24,3 +27,42 @@ print( x ** y)
 sum1 = 15
 sum2 = 2
 print(sum1//sum2)
+
+
+
+#Assaingmet Operator
+
+#sum
+sum = 15
+sum -=2
+print(sum)
+
+
+
+#comparison Operator
+
+
+x = 10
+y = 15
+#Eqal Operator
+print(x==y)
+#Not Equal
+print(x!=y)
+#Grter then
+print(x>y)
+#Lesss then
+print(x<y)
+#Grater Then Or Equal to
+print(x>=y)
+#Less Then Or equal to
+print(x<=y)
+
+
+#swaping
+
+c = 50
+d = 60
+
+c,d = d,c
+print("This value is c",c)
+print('This value is d',d)
