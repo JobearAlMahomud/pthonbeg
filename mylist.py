b0c9ad6c983c5888b8017e304fb1398f03cbdca9
JobearAlMahomud <jobear15-2174@diu.edu.bd>
2024-08-27 10:40:06 +0600

    Add list of integer, string, Boolean

--- mylist.py
+++ mylist.py
@@ -1,3 +1,19 @@
-#lilst
+#lilst integer
 li = [1,2,3]
-print(type(li))
+print(li)
+li[1] = 10
+
+print(li)
+
+# list string
+
+lis = ['Jobear','al','Mahomud']
+print(lis)
+
+lis[1] = 'James'
+print(lis)
+
+# List boolean
+
+list = [True,True,False,False]
+print(list)
